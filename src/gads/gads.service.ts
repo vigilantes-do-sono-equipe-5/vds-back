10a28e48c40264a7cc24ef6c57184ff86c5ea2e2
denerbatista <denerbatista@icloud.com>
2023-01-31 05:42:10 -0300

    feat: add methods and news dtos in gads #12

--- src/gads/gads.service.ts
+++ src/gads/gads.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@nestjs/common'
+import { Gad } from '@prisma/client'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { AddGadDto } from './dto/add-gad.dto'
+import { UpdateGadDto } from './dto/updateGad.dto'
 
 @Injectable()
 export class GadsService {
   constructor (private readonly prisma: PrismaService) {}
+
+  async create (dto: AddGadDto): Promise<Gad> {
+    return await this.prisma.gad.create({ data: { ...dto } })
+  }
+
+  async update (id: string, dto: UpdateGadDto): Promise<Gad> {
+    return await this.prisma.gad.update({ where: { id }, data: { ...dto } })
+  }
 }
