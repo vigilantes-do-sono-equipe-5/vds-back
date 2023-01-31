3310367314593b0c30b9b7546897f14458c4f18a
denerbatista <denerbatista@icloud.com>
2023-01-31 05:41:34 -0300

    feat: add methods and refactor create-user.dto in users #12

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,4 @@
-import { IsNumber } from 'class-validator'
-import { AddGadDto } from '../../gads/dto/add-gad.dto'
-import { AddIsiDto } from '../../isis/dto/add-isi.dto'
-import { AddPhqDto } from '../../phqs/dto/add-phq.dto'
-import { AddProductivityDto } from '../../productivitys/dto/add-productivity.dto'
-import { AddRatingsDto } from '../../ratings/dto/add-ratings.dto'
-import { AddSleepDiariesDto } from '../../sleep-diaries/dto/add-sleep-diaries.dto'
-import { UserProgramDto } from '../../userPrograms/dto/add-user-program.dto'
+import { IsNumber, IsString } from 'class-validator'
 
 export class CreateUserDto {
   @IsNumber()
@@ -17,11 +10,6 @@ export class CreateUserDto {
   @IsNumber()
     night_goal: number
 
-  user_program_session: UserProgramDto[]
-  sleep_diaries: AddSleepDiariesDto[]
-  ratings: AddRatingsDto[]
-  isi: AddIsiDto[]
-  gad: AddGadDto[]
-  phq: AddPhqDto[]
-  productivity: AddProductivityDto[]
+  @IsString()
+    company_id: string
 }
