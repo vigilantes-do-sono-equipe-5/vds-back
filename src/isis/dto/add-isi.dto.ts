87be3c37c9b403dda348b8af1fd74b192f10b799
denerbatista <denerbatista@icloud.com>
2023-01-31 05:42:43 -0300

    feat: add methods and news dtos in isis #12

--- src/isis/dto/add-isi.dto.ts
+++ src/isis/dto/add-isi.dto.ts
@@ -1,6 +1,12 @@
-import { IsDate, IsNumber, Min, Max } from 'class-validator'
+import { IsNumber, Min, Max, IsString } from 'class-validator'
 
 export class AddIsiDto {
+  @IsString()
+    user_id: string
+
+  @IsString()
+    company_id: string
+
   @IsNumber()
   @Min(0)
   @Max(4)
@@ -35,7 +41,4 @@ export class AddIsiDto {
   @Min(0)
   @Max(4)
     q7: number
-
-  @IsDate()
-    created_at: Date
 }
--- src/isis/isis.service.ts
+++ src/isis/isis.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@nestjs/common'
+import { Isi } from '@prisma/client'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { AddIsiDto } from './dto/add-isi.dto'
+import { UpdateIsiDto } from './dto/updateIsi.dto'
 
 @Injectable()
 export class IsisService {
   constructor (private readonly prisma: PrismaService) {}
+
+  async create (dto: AddIsiDto): Promise<Isi> {
+    return await this.prisma.isi.create({ data: { ...dto } })
+  }
+
+  async update (id: string, dto: UpdateIsiDto): Promise<Isi> {
+    return await this.prisma.isi.update({ where: { id }, data: { ...dto } })
+  }
 }
