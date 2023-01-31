2d1d331553b2b5b2b2967a336b2eb68576862e3e
denerbatista <denerbatista@icloud.com>
2023-01-31 05:46:53 -0300

    feat: add methods and change dtos in tags #12

--- src/tags/dto/create-tag.dto.ts
+++ src/tags/dto/create-tag.dto.ts
@@ -1,6 +1,43 @@
-import { IsString } from 'class-validator'
+import { IsString, ValidateIf } from 'class-validator'
 
+type TTagDto =
+  | 'caffeine'
+  | 'noise'
+  | 'pain'
+  | 'exercise'
+  | 'light'
+  | 'medicine'
+  | 'nicotine'
+  | 'partner'
+  | 'nap'
+  | 'temperature'
+  | 'meal'
+  | 'bathroom'
+  | 'alcohol'
+  | 'bathroom '
+  | 'dream'
+
+const TAGS = [
+  'caffeine',
+  'noise',
+  'pain',
+  'exercise',
+  'light',
+  'medicine',
+  'nicotine',
+  'partner',
+  'nap',
+  'temperature',
+  'meal',
+  'bathroom',
+  'alcohol',
+  'bathroom ',
+  'dream'
+]
 export class CreateTagDto {
+  @ValidateIf((_, value: string) => TAGS.includes(value))
+    sleep_tag: TTagDto
+
   @IsString()
-    sleep_tag: string
+    sleep_DiariesId: string
 }
--- src/tags/tags.service.ts
+++ src/tags/tags.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from '@nestjs/common'
+import { Tag } from '@prisma/client'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { CreateTagDto } from './dto/create-tag.dto'
+import { UpdateTagDto } from './dto/updateTag.dto'
 
 @Injectable()
 export class TagsService {
   constructor (private readonly prisma: PrismaService) {}
+
+  async create (dto: CreateTagDto): Promise<Tag> {
+    return await this.prisma.tag.create({ data: { ...dto } })
+  }
+
+  async update (dto: UpdateTagDto): Promise<Tag> {
+    return await this.prisma.tag.update({
+      where: { ...dto },
+      data: { ...dto }
+    })
+  }
 }
