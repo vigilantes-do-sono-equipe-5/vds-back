afbcd82a36029946b34a781d21912fcf758cad18
denerbatista <denerbatista@icloud.com>
2023-02-03 06:50:23 -0300

    refactor(seed): add progress in terminal and change struture #12

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,4 @@
-import {
-  PrismaClient,
-  Sleep_Diaries,
-  User_Program_Session,
-  Productivity,
-  Rating,
-  Gad,
-  Isi,
-  Phq
-} from '@prisma/client'
+import { PrismaClient, Sleep_Diaries } from '@prisma/client'
 import { CreateTagDto } from 'src/tags/dto/create-tag.dto'
 import { AddGadDto } from '../src/gads/dto/add-gad.dto'
 import { AddIsiDto } from '../src/isis/dto/add-isi.dto'
@@ -20,13 +11,40 @@ import * as data from '../src/utils/data/data.json'
 
 const prisma = new PrismaClient()
 const dataJson = [...data.user]
+let qtdUsers = 0
+let qtdUserProgramSessions = 0
+let qtdSleepDiaries = 0
+let qtdProductivity = 0
+let qtdRatings = 0
+let qtdIsi = 0
+let qtdGad = 0
+let qtdPhq = 0
 
-async function main (): Promise<void> {
+const progress = (): void => {
+  console.clear()
+  console.log(`
+      Sedding in progress...
+
+      1 company cadastrado.
+      ${qtdUsers} usuários cadastrados.
+      ${qtdUserProgramSessions} user_program_sessions cadastrados.
+      ${qtdSleepDiaries} sleep_diaries cadastrados.
+      ${qtdProductivity} productivity cadastrados.
+      ${qtdRatings} ratings cadastrados.
+      ${qtdIsi} isi cadastrados.
+      ${qtdGad} gad cadastrados.
+      ${qtdPhq} phq cadastrados.
+      `)
+}
+
+const main = async (): Promise<void> => {
   const data = {
     name: 'Empresa X'
   }
   const newCompany = await prisma.company.create({ data })
+  console.log(`Company ${newCompany.name} criado.`)
 
+  console.log(`Foram identificados ${dataJson.length} usuários.`)
   void dataJson.map(
     async ({
       points,
@@ -47,22 +65,30 @@ async function main (): Promise<void> {
         company_id: newCompany.id
       }
       const newUser = await prisma.user.create({ data })
+      qtdUsers += 1
+      progress()
 
-      user_program_sessions.map(
-        async (e: AddUserProgramDto): Promise<User_Program_Session> => {
-          const data = {
+      qtdUserProgramSessions += user_program_sessions.length
+      const dataUserProgramSessions = user_program_sessions.map(
+        (e: AddUserProgramDto) => {
+          return {
             ...e,
             user_id: newUser.id,
             company_id: newCompany.id
           }
-          return await prisma.user_Program_Session.create({ data })
         }
       )
 
+      await prisma.user_Program_Session.createMany({
+        data: dataUserProgramSessions
+      })
+      progress()
+
+      qtdSleepDiaries += sleep_diaries.length
       sleep_diaries.map(
         async (e: AddSleepDiariesDto): Promise<Sleep_Diaries> => {
           let tags: Array<{ sleep_tag: string }> | [] = []
-          e.tags ? tags = e.tags : e.tags = []
+          e.tags ? (tags = e.tags) : (e.tags = [])
           delete e.tags
 
           const data = {
@@ -75,58 +101,72 @@ async function main (): Promise<void> {
           if (tags.length > 0) {
             let i: CreateTagDto
             for (i of tags) {
-              const data = { sleep_tag: i.sleep_tag, sleep_DiariesId: sleepDiarie.id }
+              const data = {
+                sleep_tag: i.sleep_tag,
+                sleep_DiariesId: sleepDiarie.id
+              }
               await prisma.tag.create({ data })
             }
           }
           return sleepDiarie
         }
       )
+      progress()
 
-      productivity.map(async (e: AddProductivityDto): Promise<Productivity> => {
-        const data = {
+      qtdProductivity += productivity.length
+      const dataProductivity = productivity.map((e: AddProductivityDto) => {
+        return {
           ...e,
           user_id: newUser.id,
           company_id: newCompany.id
         }
-        return await prisma.productivity.create({ data })
       })
+      await prisma.productivity.createMany({ data: dataProductivity })
+      progress()
 
-      ratings.map(async (e: AddRatingsDto): Promise<Rating> => {
-        const data = {
+      qtdRatings += ratings.length
+      const dataRatings = ratings.map((e: AddRatingsDto) => {
+        return {
           ...e,
           user_id: newUser.id,
           company_id: newCompany.id
         }
-        return await prisma.rating.create({ data })
       })
+      await prisma.rating.createMany({ data: dataRatings })
+      progress()
 
-      isi.map(async (e: AddIsiDto): Promise<Isi> => {
-        const data = {
+      qtdIsi += isi.length
+      const dataIsi = isi.map((e: AddIsiDto) => {
+        return {
           ...e,
           user_id: newUser.id,
           company_id: newCompany.id
         }
-        return await prisma.isi.create({ data })
       })
+      await prisma.isi.createMany({ data: dataIsi })
+      progress()
 
-      gad.map(async (e: AddGadDto): Promise<Gad> => {
-        const data = {
+      qtdGad += gad.length
+      const dataGad = gad.map((e: AddGadDto) => {
+        return {
           ...e,
           user_id: newUser.id,
           company_id: newCompany.id
         }
-        return await prisma.gad.create({ data })
       })
+      await prisma.gad.createMany({ data: dataGad })
+      progress()
 
-      phq.map(async (e: AddPhqDto): Promise<Phq> => {
-        const data = {
+      qtdPhq += phq.length
+      const dataPhq = phq.map((e: AddPhqDto) => {
+        return {
           ...e,
           user_id: newUser.id,
           company_id: newCompany.id
         }
-        return await prisma.phq.create({ data })
       })
+      await prisma.phq.createMany({ data: dataPhq })
+      progress()
     }
   )
 }
