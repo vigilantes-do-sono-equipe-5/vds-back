c1b43e500d891fa4d18e5e56f7656468c980cf31
João Guilherme Braga <joaogbraga@hotmail.com.br>
2023-02-07 07:16:26 -0300

    fix: partial resolve error AddDto's incompatibles

--- src/phqs/phqs.service.ts
+++ src/phqs/phqs.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@nestjs/common'
 import { Phq } from '@prisma/client'
 import { PrismaService } from 'src/prisma/prisma.service'
-import { AddPhqDto } from './dto/add-phq.dto'
+/* import { AddPhqDto } from './dto/add-phq.dto' */
 import { UpdatePhqDto } from './dto/updatePhq.dto'
 
 @Injectable()
 export class PhqsService {
   constructor (private readonly prisma: PrismaService) {}
 
-  async create (dto: AddPhqDto): Promise<Phq> {
+  /*   async create (dto: AddPhqDto): Promise<Phq> {
     return await this.prisma.phq.create({ data: { ...dto } })
-  }
+  } */
 
   async update (id: string, dto: UpdatePhqDto): Promise<Phq> {
     return await this.prisma.phq.update({ where: { id }, data: { ...dto } })
