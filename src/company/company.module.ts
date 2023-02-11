e2368aa0608dd0ea0958a43a1d8eb87cd11fb564
denerbatista <denerbatista@icloud.com>
2023-02-11 20:19:04 -0300

    feat(company): add endpoints mainNumbers and chosenGoals  #15

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
+import { MethodsModule } from 'src/commonMethods/commonMethods.module'
 import { PrismaModule } from 'src/prisma/prisma.module'
 import { CompanyController } from './company.controller'
 import { CompanyService } from './company.service'
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, MethodsModule],
   controllers: [CompanyController],
   providers: [CompanyService]
 })
