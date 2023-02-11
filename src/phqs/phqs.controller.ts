23f67ca96241c4bc7d018fe992d2460ebb267603
denerbatista <denerbatista@icloud.com>
2023-02-10 21:49:49 -0300

    feat(phq): add endpoint findByCompanyId #15

--- src/phqs/phqs.controller.ts
+++ src/phqs/phqs.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Get, Param } from '@nestjs/common'
+import { AddConvertDateUTC } from 'src/commonMethods/commonMethods.interfaces'
 import { PhqsService } from './phqs.service'
 
-@Controller()
+@Controller('phq')
 export class PhqsController {
   constructor (private readonly phqsService: PhqsService) {}
+
+  @Get(':id')
+  async findByCompanyId (
+    @Param('id') id: string,
+      @Body() dto: AddConvertDateUTC
+  ): Promise<{
+        beginningAveragePhq: number
+        middleAveragePhq: number
+        endAveragePhq: number
+      }> {
+    return await this.phqsService.findByCompanyId(id, dto)
+  }
 }
--- src/phqs/phqs.service.ts
+++ src/phqs/phqs.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { Phq } from '@prisma/client'
+import { AddConvertDateUTC } from 'src/commonMethods/commonMethods.interfaces'
+import { CommonMethodsService } from 'src/commonMethods/commonMethods.service'
 import { CreateCompanyDto } from 'src/company/dto/createCompany.dto'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { CreateUserDto } from 'src/users/dto/create-user.dto'
@@ -9,7 +11,7 @@ import { UpdatePhqDto } from './dto/updatePhq.dto'
 
 @Injectable()
 export class PhqsService {
-  constructor (private readonly prisma: PrismaService) {}
+  constructor (private readonly prisma: PrismaService, private readonly commonMethodsService: CommonMethodsService) {}
 
   async create (dto: AddPhqDto): Promise<Phq> {
     const user: RelationDto<CreateUserDto> = { connect: { id: dto.user_id } }
@@ -22,5 +24,54 @@ export class PhqsService {
 
   async update (id: string, dto: UpdatePhqDto): Promise<Phq> {
     return await this.prisma.phq.update({ where: { id }, data: { ...dto } })
+  }
+
+  async findByCompanyId (
+    id: string,
+    date: AddConvertDateUTC
+  ): Promise<{
+      beginningAveragePhq: number
+      middleAveragePhq: number
+      endAveragePhq: number
+    }> {
+    const phqList = await this.prisma.phq.findMany({ where: { company_id: id } })
+
+    const dateUTC = this.commonMethodsService.divideThreePeriods(date)
+
+    const beginningPhq = phqList.filter((e: Phq) => {
+      const date = new Date(e.created_at.slice(0, 10))
+      return dateUTC ? +date >= dateUTC?.beginning.startPeriod && +date < dateUTC?.beginning.endPeriod : false
+    })
+    const beginningSumPhq = beginningPhq.map((e: Phq) => {
+      const sum = e.q1 + e.q2
+      return sum
+    })
+    const beginningAveragePhq = beginningSumPhq.reduce((a, b) => a + b, 0) / beginningSumPhq.length
+
+    const middlePhq = phqList.filter((e: Phq) => {
+      const date = new Date(e.created_at.slice(0, 10))
+      return dateUTC ? +date >= dateUTC?.middle.startPeriod && +date < dateUTC?.middle.endPeriod : false
+    })
+    const middleSumPhq = middlePhq.map((e: Phq) => {
+      const sum = e.q1 + e.q2
+      return sum
+    })
+    const middleAveragePhq = middleSumPhq.reduce((a, b) => a + b, 0) / beginningSumPhq.length
+
+    const endPhq = phqList.filter((e: Phq) => {
+      const date = new Date(e.created_at.slice(0, 10))
+      return dateUTC ? +date >= dateUTC?.end.startPeriod && +date < dateUTC?.end.endPeriod : false
+    })
+    const endSumPhq = endPhq.map((e: Phq) => {
+      const sum = e.q1 + e.q2
+      return sum
+    })
+    const endAveragePhq = endSumPhq.reduce((a, b) => a + b, 0) / endSumPhq.length
+
+    return {
+      beginningAveragePhq,
+      middleAveragePhq,
+      endAveragePhq
+    }
   }
 }
