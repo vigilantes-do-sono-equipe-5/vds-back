c9b79522846c69eceb38c82b6795b888f1b8ca5d
denerbatista <denerbatista@icloud.com>
2023-02-10 21:31:20 -0300

    feat(gad): add endpoints create, update and findByCompanyId #15

--- src/gads/gads.controller.ts
+++ src/gads/gads.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common'
+import { Gad } from '@prisma/client'
+import { AddConvertDateUTC } from 'src/commonMethods/commonMethods.interfaces'
+import { AddGadDto } from './dto/add-gad.dto'
+import { UpdateGadDto } from './dto/updateGad.dto'
 import { GadsService } from './gads.service'
 
-@Controller()
+@Controller('gad')
 export class GadsController {
   constructor (private readonly gadsService: GadsService) {}
+  @Post()
+  async create (@Body() dto: AddGadDto): Promise<Gad> {
+    return await this.gadsService.create(dto)
+  }
+
+  @Patch(':id')
+  async update (@Param('id') id: string, @Body() dto: UpdateGadDto): Promise<Gad> {
+    return await this.gadsService.update(id, dto)
+  }
+
+  @Get(':id')
+  async findByCompanyId (
+    @Param('id') id: string,
+      @Body() dto: AddConvertDateUTC
+  ): Promise<{
+        beginningAverageGad: number
+        middleAverageGad: number
+        endAverageGad: number
+      }> {
+    return await this.gadsService.findByCompanyId(id, dto)
+  }
 }
