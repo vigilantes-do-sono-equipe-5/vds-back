5ce6bc1ae7d2a3e084c0360594d14141d5ab4db9
denerbatista <denerbatista@icloud.com>
2023-02-11 14:34:46 -0300

    feat(rating): add endpoint findByCompanyId #15

--- src/ratings/ratings.controller.ts
+++ src/ratings/ratings.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Get, Param } from '@nestjs/common'
+import { AddConvertDateUTC } from 'src/commonMethods/commonMethods.interfaces'
 import { RatingsService } from './ratings.service'
 
-@Controller()
+@Controller('rating')
 export class RatingsController {
   constructor (private readonly ratingsService: RatingsService) {}
+
+  @Get(':id')
+  async findByCompanyId (
+    @Param('id') id: string,
+      @Body() date: AddConvertDateUTC
+  ): Promise<{
+        1: number
+        2: number
+        3: number
+        4: number
+        5: number
+      }> {
+    return await this.ratingsService.findByCompanyId(id, date)
+  }
 }
