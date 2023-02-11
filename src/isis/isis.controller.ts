16e60143328ff6777f2b834fbcee59d9d144021d
denerbatista <denerbatista@icloud.com>
2023-02-10 21:49:19 -0300

    feat(isi): add endpoint findByCompanyId #15

--- src/isis/isis.controller.ts
+++ src/isis/isis.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Get, Param } from '@nestjs/common'
+import { AddConvertDateUTC } from 'src/commonMethods/commonMethods.interfaces'
 import { IsisService } from './isis.service'
 
-@Controller()
+@Controller('isi')
 export class IsisController {
   constructor (private readonly isisService: IsisService) {}
+
+  @Get(':id')
+  async findByCompanyId (
+    @Param('id') id: string,
+      @Body() dto: AddConvertDateUTC
+  ): Promise<{
+        beginningAverageIsi: number
+        middleAverageIsi: number
+        endAverageIsi: number
+      }> {
+    return await this.isisService.findByCompanyId(id, dto)
+  }
 }
--- src/isis/isis.module.ts
+++ src/isis/isis.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
+import { MethodsModule } from 'src/commonMethods/commonMethods.module'
 import { PrismaModule } from 'src/prisma/prisma.module'
 import { IsisController } from './isis.controller'
 import { IsisService } from './isis.service'
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, MethodsModule],
   controllers: [IsisController],
   providers: [IsisService]
 })
