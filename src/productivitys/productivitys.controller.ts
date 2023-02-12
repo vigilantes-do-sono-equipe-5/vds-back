7119d0bdbb2cfc2522b48293448519d374ec3c34
João Guilherme Braga <joaogbraga@hotmail.com.br>
2023-02-12 09:29:57 -0300

    feat(productivity): add endpoints create and patch #15

--- src/productivitys/productivitys.controller.ts
+++ src/productivitys/productivitys.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Post, Patch, Body, Param } from '@nestjs/common'
+import { Productivity } from '@prisma/client'
+import { AddProductivityDto } from './dto/add-productivity.dto'
+import { UpdateProductivityDto } from './dto/updateProductivity.dto'
 import { ProductivitysService } from './productivitys.service'
 
 @Controller()
 export class ProductivitysController {
   constructor (private readonly productivitysService: ProductivitysService) {}
+  @Post()
+  async create (@Body() dto: AddProductivityDto): Promise<Productivity> {
+    return await this.productivitysService.create(dto)
+  }
+
+  @Patch(':id')
+  async update (@Param('id') id: string, @Body() dto: UpdateProductivityDto): Promise<Productivity> {
+    return await this.productivitysService.update(id, dto)
+  }
 }
