4025702bd2ff59eac3ef18fb4df0b26c36d140cf
João Guilherme Braga <joaogbraga@hotmail.com.br>
2023-02-12 09:34:21 -0300

    feat(tags): add endpoints create and patch #15

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Post, Patch, Body, Param } from '@nestjs/common'
+import { Tag } from '@prisma/client'
+import { CreateTagDto } from './dto/create-tag.dto'
+import { UpdateTagDto } from './dto/updateTag.dto'
 import { TagsService } from './tags.service'
 
 @Controller()
 export class TagsController {
   constructor (private readonly tagsService: TagsService) {}
+  @Post()
+  async create (@Body() dto: CreateTagDto): Promise<Tag> {
+    return await this.tagsService.create(dto)
+  }
+
+  @Patch(':id')
+  async update (@Param('id') id: string, @Body() dto: UpdateTagDto): Promise<Tag> {
+    return await this.tagsService.update(id, dto)
+  }
 }
