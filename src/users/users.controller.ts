9c833511409b11c0170c5c432194e99f930432ce
João Guilherme Braga <joaogbraga@hotmail.com.br>
2023-02-12 09:33:29 -0300

    feat(users): add endpoints create and patch #15

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Post, Patch, Body, Param } from '@nestjs/common'
+import { User } from '@prisma/client'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
 import { UsersService } from './users.service'
 
 @Controller()
 export class UsersController {
   constructor (private readonly nameService: UsersService) {}
+  @Post()
+  async create (@Body() dto: CreateUserDto): Promise<User> {
+    return await this.nameService.create(dto)
+  }
+
+  @Patch(':id')
+  async update (@Param('id') id: string, @Body() dto: UpdateUserDto): Promise<User> {
+    return await this.nameService.update(id, dto)
+  }
 }
