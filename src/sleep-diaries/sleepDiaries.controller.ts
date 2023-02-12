b444e12f40e8dd295ce231e1bd21abd268ff84a0
João Guilherme Braga <joaogbraga@hotmail.com.br>
2023-02-12 09:31:42 -0300

    feat(sleep-diaries): add endpoints create and patch #15

--- src/sleep-diaries/sleepDiaries.controller.ts
+++ src/sleep-diaries/sleepDiaries.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Post, Patch, Param, Body } from '@nestjs/common'
+import { Sleep_Diaries } from '@prisma/client'
+import { AddSleepDiariesWithTagsDto } from './dto/add-sleepDiaries.dto'
+import { UpdateSleepDiariesDto } from './dto/updateSleepDiaries.dto'
 import { SleepDiariesService } from './sleepDiaries.service'
 
 @Controller()
 export class SleepDiariesController {
   constructor (private readonly sleepDiariesService: SleepDiariesService) {}
+  @Post()
+  async create (@Body() dto: AddSleepDiariesWithTagsDto): Promise<Sleep_Diaries | null> {
+    return await this.sleepDiariesService.create(dto)
+  }
+
+  @Patch(':id')
+  async update (@Param('id') id: string, @Body() dto: UpdateSleepDiariesDto): Promise<Sleep_Diaries> {
+    return await this.sleepDiariesService.update(id, dto)
+  }
 }
