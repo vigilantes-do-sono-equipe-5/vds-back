c34f0ad6f44c382563b74d2ef6bee69197c661a0
denerbatista <denerbatista@icloud.com>
2023-02-11 22:30:35 -0300

    feat(sleepDiaries): add endpoint interferenceAndTechniques  #15

--- src/sleep-diaries/sleepDiaries.controller.ts
+++ src/sleep-diaries/sleepDiaries.controller.ts
@@ -1,7 +1,44 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Param } from '@nestjs/common'
 import { SleepDiariesService } from './sleepDiaries.service'
 
-@Controller()
+@Controller('sleepDiaries')
 export class SleepDiariesController {
   constructor (private readonly sleepDiariesService: SleepDiariesService) {}
+
+  @Get('interferenceAndTechniques/:id')
+  async sleepInterferenceAndTechniques (@Param('id') id: string): Promise<{ moreAppliedTechniques: { autogenic_training: number
+    behavior_activation: number
+    challenge_catastrophic_thinking: number
+    deep_breath: number
+    gratitude: number
+    imagery: number
+    light_therapy: number
+    meditation: number
+    paradoxical_intention: number
+    parking_lot: number
+    pmr: number
+    stimulus_control: number
+    thought_block: number
+  }
+  sleepInterference: {
+    nicotine: number
+    nap: number
+    exercise: number
+    caffeine: number
+    partner: number
+    pain: number
+    medicine: number
+    noise: number
+    bathroom: number
+    meal: number
+    temperature: number
+    alcohol: number
+    light: number
+    dream: number }
+  }> {
+    return {
+      ...await this.sleepDiariesService.sleepInterference(id),
+      ...await this.sleepDiariesService.moreAppliedTechniques(id)
+    }
+  }
 }
