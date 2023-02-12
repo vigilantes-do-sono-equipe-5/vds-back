2aed30a4c13ca92df9136f8d51cb94db832cb264
João Guilherme Braga <joaogbraga@hotmail.com.br>
2023-02-12 09:30:24 -0300

    feat(rating): add endpoints create and patch #15

--- src/ratings/ratings.controller.ts
+++ src/ratings/ratings.controller.ts
@@ -1,10 +1,22 @@
-import { Body, Controller, Get, Param } from '@nestjs/common'
+import { Body, Controller, Get, Post, Patch, Param } from '@nestjs/common'
+import { Rating } from '@prisma/client'
 import { AddConvertDateUTC } from 'src/commonMethods/commonMethods.interfaces'
+import { AddRatingsDto } from './dto/add-ratings.dto'
+import { UpdateRatingsDto } from './dto/updateRating.dto'
 import { RatingsService } from './ratings.service'
 
 @Controller('rating')
 export class RatingsController {
   constructor (private readonly ratingsService: RatingsService) {}
+  @Post()
+  async create (@Body() dto: AddRatingsDto): Promise<Rating> {
+    return await this.ratingsService.create(dto)
+  }
+
+  @Patch(':id')
+  async update (@Param('id') id: string, @Body() dto: UpdateRatingsDto): Promise<Rating> {
+    return await this.ratingsService.update(id, dto)
+  }
 
   @Get(':id')
   async findByCompanyId (
