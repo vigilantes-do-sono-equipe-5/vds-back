172d3f70892a48524786a61406ec68f4ba4f11ef
João Guilherme Braga <joaogbraga@hotmail.com.br>
2023-02-12 09:29:15 -0300

    feat(phq): add endpoints create and patch #15

--- src/phqs/phqs.controller.ts
+++ src/phqs/phqs.controller.ts
@@ -1,10 +1,22 @@
-import { Body, Controller, Get, Param } from '@nestjs/common'
+import { Body, Controller, Get, Post, Patch, Param } from '@nestjs/common'
+import { Phq } from '@prisma/client'
 import { AddConvertDateUTC } from 'src/commonMethods/commonMethods.interfaces'
+import { AddPhqDto } from './dto/add-phq.dto'
+import { UpdatePhqDto } from './dto/updatePhq.dto'
 import { PhqsService } from './phqs.service'
 
 @Controller('phq')
 export class PhqsController {
   constructor (private readonly phqsService: PhqsService) {}
+  @Post()
+  async create (@Body() dto: AddPhqDto): Promise<Phq> {
+    return await this.phqsService.create(dto)
+  }
+
+  @Patch(':id')
+  async update (@Param('id') id: string, @Body() dto: UpdatePhqDto): Promise<Phq> {
+    return await this.phqsService.update(id, dto)
+  }
 
   @Get(':id')
   async findByCompanyId (
